ed56a32c5a0cdecd10b6135652cb7025f61fd557
Mina Dawood <mina.latif@live.com>
2022-12-24 14:13:56 -0500

    OpenAI frontend

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 const app = express();
 
 // Enable body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/openai', require('./routes/openaiRoutes'));
 
